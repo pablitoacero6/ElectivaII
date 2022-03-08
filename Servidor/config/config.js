6e68f3ac77280895a7c071edb7c0002bbf270a3a
pablitoacero6 <51001188+pablitoacero6@users.noreply.github.com>
2022-03-08 10:36:25 -0500

    conexión creación cliente

--- Servidor/config/config.js
+++ Servidor/config/config.js
@@ -6,6 +6,14 @@ DB = {
     connectString: 'localhost:1521'
 }
 
+oracledb.getConnection(DB, function(err, connection){{
+    if(err){
+        console.error("db has connected failed: "+ err.message)
+        return ;
+    }
+    console.log("DB has connected")
+}});
+
 async function open(sql, binds, autoCommit){
     let con = await oracledb.getConnection(DB);
     let result = await con.execute(sql,binds,{autoCommit});
